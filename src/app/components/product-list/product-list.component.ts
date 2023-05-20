3005f89f2578915589912d8a6d00ac243a8ffe5b
kamisc <kamil.seweryn@o2.pl>
2023-05-20 18:51:23 +0200

    add to cart functionality

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -89,4 +89,10 @@ export class ProductListComponent implements OnInit {
       this.theTotalElements = data.page.totalElements;
     }
   }
+
+  addToCart(theProduct: Product) {
+    console.log(`Adding to cart: ${theProduct.name}, ${theProduct.unitPrice}`);
+
+    // TODO
+  }
 }
