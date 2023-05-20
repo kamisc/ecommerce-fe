5d3b9e904bff7766bc70385c5cce974a85fb1368
kamisc <kamil.seweryn@o2.pl>
2023-05-20 19:15:28 +0200

    add cart-service and handle addToCart method and comupte totalPrice and totalQuantity attr

---
+++ src/app/services/cart.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@angular/core';
+import { CartItem } from '../common/cart-item';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+
+  cartItems: CartItem[] = [];
+
+  totalPrice: Subject<number> = new Subject<number>();
+  totalQuantity: Subject<number> = new Subject<number>();
+
+  constructor() { }
+
+  addToCart(theCartItem: CartItem) {
+    let alreadyExistInCart: boolean = false;
+    let existingCartItem: CartItem = undefined;
+
+    if (this.cartItems.length > 0) {
+      for (let cartItem of this.cartItems) {
+        if (cartItem.id === theCartItem.id) {
+          existingCartItem = cartItem;
+          break;
+        }
+      }
+      alreadyExistInCart = (existingCartItem != undefined);
+    }
+
+    if (alreadyExistInCart) {
+      existingCartItem.quantity++;
+    } else {
+      this.cartItems.push(theCartItem);
+    }
+
+    this.computeCartTotals();
+  }
+
+  private computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQuantityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems) {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitPrice;
+      totalQuantityValue += currentCartItem.quantity;
+    }
+
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQuantityValue);
+
+    this.logCartData(totalPriceValue, totalQuantityValue);
+  }
+
+  private logCartData(totalPriceValue: number, totalQuantityValue: number) {
+    console.log(`Contents of the cart`);
+    for (let cartItem of this.cartItems) {
+      const subTotalPrice = cartItem.quantity * cartItem.unitPrice;
+      console.log(`name: ${cartItem.name}, quantity = ${cartItem.quantity}, unitPrice = ${cartItem.unitPrice}`);
+    }
+
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQuantityValue}`);
+    console.log('-------');
+  }
+}
