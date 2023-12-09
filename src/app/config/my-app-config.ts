0f2c688cdf5b6367f34934a325aadf34f34ea8ea
kamisc <kamil.seweryn@o2.pl>
2023-12-09 13:11:19 +0100

    add test https config with self signed cert

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -2,7 +2,7 @@ export default {
     oidc: {
         clientId: '0oabi5gx3wgFYdIMg5d7',
         issuer: 'https://dev-31020962.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }   
 }
